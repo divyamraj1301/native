579da4095d7c7986c0de2df27c16d3662d93a85e
divyamraj1301 <divyamraj1301@gmail.com>
2024-03-01 11:05:14 +0530

    initialise

--- App.js
+++ App.js
@@ -1,20 +1,82 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import {
+  Text,
+  View,
+  StyleSheet,
+  TouchableOpacity,
+  StatusBar,
+  TextInput,
+  FlatList,
+} from "react-native";
 
-export default function App() {
+const App = () => {
+  const [todo, setTodo] = useState("");
+  const [todos, setTodos] = useState([]);
+
+  const handleAddTodo = () => {
+    if (!todo) return;
+    setTodos([...todos, { id: Date.now(), text: todo }]);
+    setTodo("");
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.heading}>Todo List in Native</Text>
+      <View style={styles.inputContainer}>
+        <TextInput
+          onChangeText={(text) => setTodo(text)}
+          value={todo}
+          placeholder="Enter a task"
+          style={styles.input}
+        />
+        <TouchableOpacity onPress={handleAddTodo}>
+          <Text style={styles.button}>Go</Text>
+        </TouchableOpacity>
+      </View>
+      <View>
+        <FlatList
+          data={todos}
+          renderItem={({ item }) => <Text>{item.text}</Text>}
+          keyExtractor={(item) => item.id.toSring}
+        />
+      </View>
       <StatusBar style="auto" />
     </View>
   );
-}
+};
+
+export default App;
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    backgroundColor: "#e6f0ff",
+  },
+  heading: {
+    marginVertical: 10,
+    fontSize: 30,
+    fontWeight: "700",
+  },
+  inputContainer: {
+    flexDirection: "row",
+    marginHorizontal: 10,
+    alignItems: "center",
+  },
+  input: {
+    flex: 1,
+    backgroundColor: "white",
+    shadowColor: "black",
+    elevation: 10,
+    marginRight: 5,
+    paddingHorizontal: 20,
+    paddingVertical: 10,
+    borderRadius: 50,
+  },
+  button: {
+    padding: 13,
+    backgroundColor: "white",
+    borderRadius: 50,
+    elevation: 10,
   },
 });
